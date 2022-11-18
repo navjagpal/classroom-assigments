d726e5394d6d492f9c4fe9b745987c7bf8feeee2
Nav Jagpal <nav@gmail.com>
2022-11-18 19:01:37 +0000

    Add filename arguments.

--- assignments.py
+++ assignments.py
@@ -45,55 +45,58 @@ FEATURE_COLUMNS = {
 SOLVER_TIME_LIMIT_SECONDS = 60
 
 
-def GetStudents():
+def GetStudents(students_file):
   students = {}
-  with open('students.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      students[row['id']] = row
+  reader = csv.DictReader(students_file)
+  for row in reader:
+    students[row['id']] = row
   return students
 
 
 parser = argparse.ArgumentParser(description='Assign students to classrooms.')
+parser.add_argument('--students_file', type=open, required=True)
+parser.add_argument('--assignments_file',
+  type=argparse.FileType('w', encoding='UTF-8'), required=True)
+parser.add_argument('--classrooms_file',
+  type=argparse.FileType('w', encoding='UTF-8'), required=True)
 args = parser.parse_args()
 
 
 def main():
-  students = GetStudents()
+  students = GetStudents(args.students_file)
   num_classes = 10
   classes_to_students = assignments_lib.generate_assignments(
     students, num_classes, FEATURE_COLUMNS)
 
-  with open('assignments.csv', 'w', newline='') as assignments_csvfile:
-    assignments_writer = csv.writer(assignments_csvfile, delimiter=',')
-    assignments_writer.writerow(['Classroom', 'StudentNumber', 'Name'] + list(FEATURE_COLUMNS.keys()))
+  assignments_writer = csv.writer(args.assignments_file, delimiter=',')
+  assignments_writer.writerow(['Classroom', 'StudentNumber', 'Name'] + list(FEATURE_COLUMNS.keys()))
 
-    with open('classrooms.csv', 'w', newline='') as classrooms_csvfile:
-      classrooms_writer = csv.writer(classrooms_csvfile, delimiter=',')
-      classrooms_writer.writerow(['Classroom', 'Size'] + list(FEATURE_COLUMNS.keys()))
-    
-      print(classes_to_students)
-      for i in range(num_classes):
-        print('%d: %d' % (i, len(classes_to_students[i])))
-        features = {}
+  with open('classrooms.csv', 'w', newline='') as classrooms_csvfile:
+    classrooms_writer = csv.writer(classrooms_csvfile, delimiter=',')
+    classrooms_writer.writerow(['Classroom', 'Size'] + list(FEATURE_COLUMNS.keys()))
+  
+    print(classes_to_students)
+    for i in range(num_classes):
+      print('%d: %d' % (i, len(classes_to_students[i])))
+      features = {}
+      for feature in FEATURE_COLUMNS:
+        features[feature] = 0
+      for sid in classes_to_students[i]:
+        row = [i, sid, students[sid]['name']]
         for feature in FEATURE_COLUMNS:
-          features[feature] = 0
-        for sid in classes_to_students[i]:
-          row = [i, sid, students[sid]['name']]
-          for feature in FEATURE_COLUMNS:
-            column = FEATURE_COLUMNS[feature]['Column']
-            value = FEATURE_COLUMNS[feature]['Value']
-            if students[sid][column] == value:
-              features[feature] += 1
-              row.append(1)
-            else:
-              row.append(0)
-          assignments_writer.writerow(row)
-        row = [i, len(classes_to_students[i])]
-        for feature in features:
-          print('\t%s:%d' % (feature, features[feature]))
-          row.append(features[feature])
-        classrooms_writer.writerow(row)
+          column = FEATURE_COLUMNS[feature]['Column']
+          value = FEATURE_COLUMNS[feature]['Value']
+          if students[sid][column] == value:
+            features[feature] += 1
+            row.append(1)
+          else:
+            row.append(0)
+        assignments_writer.writerow(row)
+      row = [i, len(classes_to_students[i])]
+      for feature in features:
+        print('\t%s:%d' % (feature, features[feature]))
+        row.append(features[feature])
+      classrooms_writer.writerow(row)
 
 
 if __name__ == '__main__':
