b86d5c4345e5d06620a6f0888f16e33262819f13
Nav Jagpal <nav@gmail.com>
2022-11-22 15:14:45 +0000

    Add CORS to GCP function, get v0 of simple web form working.

--- main.py
+++ main.py
@@ -18,11 +18,29 @@ def generate_assignments(request):
         Functions, see the `Writing HTTP functions` page.
         <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
     """
-    feature_values = request.json["feature_values"]
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows POST requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    feature_values = request.json['feature_values']
     students = {}
-    for student in request.json["students"]:
-        students[student["id"]] = student
-    num_classes = request.json["num_classes"]
+    for student in request.json['students']:
+        students[student['id']] = student
+    num_classes = request.json['num_classes']
     classes_to_students = assignments_lib.generate_assignments(
         students, num_classes, feature_values, 30)
-    return classes_to_students
+    return (classes_to_students, 200, headers)
